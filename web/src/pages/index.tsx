34388542db59327f22053dcd44566404e09249b7
Eric Kivel <ericvkivel1@gmail.com>
2022-04-24 22:03:19 -0300

    feat: graphQL setup

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,26 +1,26 @@
-import { getAccessToken, useUser } from "@auth0/nextjs-auth0";
+import { getAccessToken, getSession, useUser } from "@auth0/nextjs-auth0";
 import { GetServerSideProps } from "next";
 
 export default function Home() {
-  const { user } = useUser();
-  
-  return (
-    <div>
-      <h1>Eric Kivel</h1>
-
-      <pre>{JSON.stringify(user, null, 2)}</pre>
-
-      <a href="/api/auth/login">Login</a>
-    </div>
-  )
+  return null;
 }
 
 export const getServerSideProps: GetServerSideProps = async ({req, res}) => {
-  const token = getAccessToken(req, res);
-
-  console.log(token);
+  const session = getSession(req, res);
   
-  return {
-    props: {},
-  }
+  if (!session) {
+    return {
+      redirect: {
+        destination: '/api/auth/login',
+        permanent: false,
+      },
+    }
+  } else {
+    return {
+      redirect: {
+        destination: '/app',
+        permanent: false,
+      },
+    };
+  };
 };
